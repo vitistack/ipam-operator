6cacb382e78ee95395cbc920c55631e88e2cc52b
Frode Eriksen <frode@eriksen.casa>
2025-06-13 08:05:56 +0200

    fixed ip-family both

--- internal/webhook/v1/service_webhook.go
+++ internal/webhook/v1/service_webhook.go
@@ -274,7 +274,7 @@ func (d *ServiceCustomDefaulter) Default(ctx context.Context, obj runtime.Object
 	case "both":
 		if !strings.Contains(annotations["ipam.vitistack.io/addresses"], ".") {
 			servicelog.Info("Request IPv4-address for Service:", "name", service.GetName())
-			requestIPv6AddrObject.IpFamily = "ipv4"
+			requestIPv4AddrObject.IpFamily = "ipv4"
 			responseIPv4AddrObject, err = utils.RequestIP(requestIPv4AddrObject)
 			if err != nil {
 				servicelog.Info("Request IPv4-address failed!", "name", service.GetName(), "Message", err)
